7853d3380e908a958505642c87c6d97d0e8eec0b
miloserdow <miloserdow.artem@gmail.com>
2025-11-12 02:40:03 +0300

    исправил кнопку покупки baseIncome

--- game-client.js
+++ game-client.js
@@ -318,7 +318,7 @@ class ClickerGameClient {
                     <div class="upgrade-description">${u.description}</div>
                     <button class="buy-btn" ${u.canAfford ? '' : 'disabled'} 
                             onclick="game.buyPassiveUpgrade(${u.id})">
-                        Купить (+${u.income}/сек)
+                        Купить (+${u.baseIncome}/сек)
                     </button>
                 </div>
             `;
